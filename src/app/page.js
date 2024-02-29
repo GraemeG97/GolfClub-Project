7439fe73f5736d5c41ca6af95cb329b032f7509d
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-02-29 14:31:28 +0000

    changed logo size

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center">
-        <div className="absolute h-[300px] w-full sm:w-[480px] -translate-x-1/6 rounded-full bg-gradient-radial from-white to-transparent blur-2xl content-[''] dark:bg-gradient-to-br dark:from-transparent dark:to-yellow-700 dark:opacity-10 lg:h-[360px] z-[-1]"></div>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff90]"
           src="/golfclub4project.svg"
           alt="Golf Club Logo"
-          width={400}
+          width={200}
           height={10}
           priority
         />
