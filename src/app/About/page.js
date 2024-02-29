05f1e4c47d105d4a86f6a7d93cb68b31399763a5
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-02-29 14:43:53 +0000

    added description about page

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,7 +1,45 @@
 export default function Page() {
   return (
-    <>
-      <h1>This is where I will put explanations about the website</h1>
-    </>
+    <div>
+      <h2>About Us: Discover the Finest Golf Clubs</h2>
+      <p>
+        Welcome to our premier destination for discerning golfers seeking the
+        epitome of excellence in golf equipment. At our website, we curate and
+        showcase some of the finest golf clubs ever produced, catering to golf
+        enthusiasts who demand nothing but the best for their game.
+      </p>
+      <h3>Our Mission:</h3>
+      <p>
+        We are driven by a passion for the sport of golf and a commitment to
+        excellence. Our mission is to provide golfers with access to the most
+        exceptional golf clubs available, offering insights, reviews, and
+        recommendations to elevate their game to new heights.
+      </p>
+      <h3>The Essence of Quality:</h3>
+      <p>
+        We understand that the right golf club can make all the difference
+        between a good game and an exceptional one. That's why we meticulously
+        select only the highest quality golf clubs from renowned manufacturers
+        and craftsmen around the world. From forged irons to
+        precision-engineered drivers, each club featured on our website embodies
+        the perfect balance of performance, technology, and craftsmanship.
+      </p>
+      <h3>Join Our Community:</h3>
+      <p>
+        Become a part of our vibrant golfing community and stay connected with
+        fellow enthusiasts. Follow us on social media for the latest updates,
+        tips, and exclusive offers. Share your experiences, engage with
+        like-minded golfers, and elevate your passion for the game.
+      </p>
+      <h3>Elevate Your Game:</h3>
+      <p>
+        At our website, we believe that every golfer deserves access to the
+        finest equipment to help them achieve their full potential on the
+        course. Explore our collection of premium golf clubs and experience the
+        difference for yourself. Whether you're striving for precision, power,
+        or performance, we have the perfect club to take your game to the next
+        level.
+      </p>
+    </div>
   );
 }
---
+++ src/app/Golf Clubs/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Some Of The Best Golf Clubs Produced</h2>
+    </div>
+  );
+}
