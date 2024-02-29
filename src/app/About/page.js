f7d390bea72d8417a21558261f5b7a6a7b4ecb54
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-02-29 14:28:10 +0000

    logo and first commit

---
+++ src/app/About/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1>This is where I will put explanations about the website</h1>
+    </>
+  );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <div>
+      <ol className="flex flex-row p-3">
+        <li className="m-4">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="m-4">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="m-4">
+          <Link href="add-golf-club">Add Your Fav Golf Club</Link>
+        </li>
+      </ol>
+    </div>
+  );
+}
