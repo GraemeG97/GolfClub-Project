b35dc2876e79f1e22159747c8ddc549d0c69ea08
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-04 08:41:56 +0000

    added some basic styling. coudln't get posts

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,22 +1,24 @@
 export default function Page() {
   return (
-    <div>
-      <h2>About Us: Discover the Finest Golf Clubs</h2>
-      <p>
+    <div className="flex justify-center flex-col items-center">
+      <h2 className="text-center font-bold mb-4">
+        About Us: Discover the Finest Golf Clubs
+      </h2>
+      <p className="text-center mb-4">
         Welcome to our premier destination for discerning golfers seeking the
         epitome of excellence in golf equipment. At our website, we curate and
         showcase some of the finest golf clubs ever produced, catering to golf
         enthusiasts who demand nothing but the best for their game.
       </p>
-      <h3>Our Mission:</h3>
-      <p>
+      <h3 className="text-center font-bold mb-4">Our Mission:</h3>
+      <p className="text-center mb-4">
         We are driven by a passion for the sport of golf and a commitment to
         excellence. Our mission is to provide golfers with access to the most
         exceptional golf clubs available, offering insights, reviews, and
         recommendations to elevate their game to new heights.
       </p>
-      <h3>The Essence of Quality:</h3>
-      <p>
+      <h3 className="text-center font-bold mb-4">The Essence of Quality:</h3>
+      <p className="text-center mb-4">
         We understand that the right golf club can make all the difference
         between a good game and an exceptional one. That is why we meticulously
         select only the highest quality golf clubs from renowned manufacturers
@@ -24,15 +26,15 @@ export default function Page() {
         precision-engineered drivers, each club featured on our website embodies
         the perfect balance of performance, technology, and craftsmanship.
       </p>
-      <h3>Join Our Community:</h3>
-      <p>
+      <h3 className="text-center font-bold mb-4">Join Our Community:</h3>
+      <p className="text-center mb-4">
         Become a part of our vibrant golfing community and stay connected with
         fellow enthusiasts. Follow us on social media for the latest updates,
         tips, and exclusive offers. Share your experiences, engage with
         like-minded golfers, and elevate your passion for the game.
       </p>
-      <h3>Elevate Your Game:</h3>
-      <p>
+      <h3 className="text-center font-bold mb-4">Elevate Your Game:</h3>
+      <p className="text-center mb-4">
         At our website, we believe that every golfer deserves access to the
         finest equipment to help them achieve their full potential on the
         course. Explore our collection of premium golf clubs and experience the
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-center min-h-screen p-6">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Welcome to SwingStickCritic
+      </h1>
+      <p className="text-lg text-center max-w-lg">
+        This is a place where we can look back on the past and still be in the
+        present. I have picked out 9 golf clubs: 3 Drivers, 3 Irons & 3 Putters
+        which I personally like. Click Golf Clubs above to check each one out!
+      </p>
+    </main>
   );
 }
